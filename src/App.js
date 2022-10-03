03188bc002103612f694d6df9c775295e90cc574
Sriniketh J <srinikethcr7@gmail.com>
2022-10-03 23:32:05 +0530

    added: week-1 progress

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Crud from "./crud";
+import Update from "./crud/Update";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ToastContainer />
+      <Routes>
+        <Route exact path="/" element={<Crud />} />
+        <Route exact path="/update/:id" element={<Update />} />
+      </Routes>
     </div>
   );
 }
